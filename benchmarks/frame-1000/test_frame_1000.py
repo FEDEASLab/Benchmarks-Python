fd5d0338adb390b61d0a1ac6ceb33c218e14d700
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-09-18 18:44:42 -0700

    Update test_frame_1000.py

--- benchmarks/frame-1000/test_frame_1000.py
+++ benchmarks/frame-1000/test_frame_1000.py
@@ -1,10 +1,18 @@
+#
+# Example from [1] 
+#
+# [1] C. M. Perez and F. C. Filippou, 
+#     "On nonlinear geometric transformations of finite elements,"
+#     Numerical Meth Engineering, vol. 125, no. 17, Sep. 2024, 
+#     doi: 10.1002/nme.7506.
+#
 import xara
 from xara.rotations import exp, log
 import numpy as np
 import pytest
 
 
-def create_model(element, transform, R=None):
+def Ex_4_1_Model(element, transform, R=None):
     model = xara.Model(ndm=3, ndf=6)
     L = 1.0
     if R is None:
@@ -32,9 +40,10 @@ def create_model(element, transform, R=None):
 
     return model
 
-def run(element, transform):
+
+def Ex_4_1_Case(element, transform):
     print(transform)
-    model = create_model(element, transform)
+    model = Ex_4_1_Model(element, transform, np.eye(3))
     model.analysis("Static")
     model.analyze(1)
 
@@ -43,25 +52,24 @@ def run(element, transform):
     for i in range(2):
         model.setNodeDisp(i+1, [0]*3 + Ri[i])
 
-    # model.print()
     model.analysis("Static")
     model.analyze(1, operation="update")
     e1 = model.state.element(1).section(1).strain()[0:7]
-    print(e1)
+    print(" A: ", e1)
 
 
     R = exp([0.2, 1.2, -0.5])
-    model = create_model(element, transform)
+    model = Ex_4_1_Model(element, transform)
     model.analysis("Static")
     model.analyze(1)
-    # for i in range(2):
-    #     RI = log((R@exp(Ri[i]))).tolist()
-    #     model.setNodeDisp(i+1, [0]*3 + RI)
+
+    #
+    #
+    #
     X0 = [
         np.array([0.0, 0.0, 0.0]),
         np.array([1.0, 0.0, 0.0]) # L = 1.0
     ]
-
     for i in range(2):
         # 1. Update the rotations
         RI = log((R @ exp(Ri[i]))).tolist()
@@ -77,14 +85,14 @@ def run(element, transform):
     model.analysis("Static")
     model.analyze(1, operation="update")
     e2 = model.state.element(1).section(1).strain()[0:7]
-    print(e2)
+    print(" B: ", e2)
 
     if element == "ExactFrame" or transform == "Identity":
         assert e1[3] == pytest.approx(-1.274645, abs=1e-5) # torsion
         assert e2[3] == pytest.approx(-1.263986, abs=1e-5) #
         assert e1[4] == pytest.approx( 1.267559, abs=1e-5) # torsion
 
-    elif element in {"CosseratFrame", "CosseratFrame01"} and transform == "Spherical":
+    elif "CosseratFrame" in element and transform == "Spherical":
         assert e1[3] == pytest.approx(e2[3], rel=1e-12)
     else:
         raise ValueError()
@@ -92,15 +100,20 @@ def run(element, transform):
 
 
 def test_frame_1000_simo():
-    run("ExactFrame", "Linear")
-    run("ExactFrame", "Identity")
-    run("ExactFrame", "Spherical")
+    Ex_4_1_Case("ExactFrame", "Linear")
+    Ex_4_1_Case("ExactFrame", "Identity")
+    Ex_4_1_Case("ExactFrame", "Spherical")
 
 
 
 def test_frame_1000_jelenic():
-    run("CosseratFrame",   "Identity")
-    run("CosseratFrame",   "Spherical")
-    run("CosseratFrame01", "Spherical")
+    Ex_4_1_Case("CosseratFrame",   "Identity")
+    Ex_4_1_Case("CosseratFrame",   "Spherical")
+    Ex_4_1_Case("CosseratFrame01", "Spherical")
+    Ex_4_1_Case("CosseratFrame02", "Spherical")
 
 
+
+if __name__ == "__main__":
+    test_frame_1000_simo()
+    test_frame_1000_jelenic()
