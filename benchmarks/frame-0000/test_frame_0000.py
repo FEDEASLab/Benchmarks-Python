738000230112d1e7d646a57f7724e76f3dc2f4c1
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-09-19 04:18:06 -0700

    Update test_frame_0000.py

--- benchmarks/frame-0000/test_frame_0000.py
+++ benchmarks/frame-0000/test_frame_0000.py
@@ -24,6 +24,10 @@ def test_constructors():
                               n=8, 
                               gauss_type="Legendre"
                 )
+            case "b":
+                # TODO
+                pass
+
 
 
     for syntax in {"a"}:
@@ -50,13 +54,10 @@ def test_constructors():
         assert ev[1] == pytest.approx(29854.153, rel=1e-6), ev[1]
 
 
-
-def test_transform():
-    """
-    Ensure graceful handling of invalid geometric transformation vectors 
-    in 3D frame elements.
-    This segfaults upstream.
-    """
+@pytest.mark.parametrize("element", [
+    "PrismFrame", "ForceFrame", "ExactFrame", "forceBeamColumn"
+])
+def test_transform(element):
     # Set up the model
     model = xara.Model(ndm=3, ndf=6)
 
@@ -78,22 +79,119 @@ def test_transform():
 
     model.section("ElasticFrame", 1, E=E, A=A, Iz=Iz, Iy=Iy, G=G, J=J)
     # define elements
-    ele_type = "PrismFrame"
-    # tag *[ndI ndJ]  A  E  G  Jx  Iy   Iz  transfOBJs
-    #osp.element(ele_type, 1, (1, 2), A, E, G, J, Iz, Iy, 1)
-    #osp.element(ele_type, 2, (2, 3), A, E, G, J, Iz, Iy, 2)     # <-- crashes
 
-    ele_type = "forceBeamColumn"
-    model.element(ele_type, 1, (1, 2), section=1, transform=1)
+    model.element(element, 1, (1, 2), section=1, transform=1)
     try:
-        model.element(ele_type, 2, (2, 3), section=1, transform=2)           # <-- crashes
+        model.element(element, 2, (2, 3), section=1, transform=2)
     except:
+        assert True
         model.wipe()
         return 
     
     raise Exception("Expected error not raised")
 
 
+@pytest.mark.parametrize("element", [
+    "PrismFrame", "ForceFrame", "ExactFrame", "forceBeamColumn"
+])
+def test_transform_missing(element):
+    # Set up the model
+    model = xara.Model(ndm=3, ndf=6)
 
-# if __name__ == "__main__":
-#     test_constructors()
+    model.node(1, 0, 0, 0)
+    model.node(2, 0, 5, 0)
+    model.node(3, 0, 5, 5)
+
+    fixed = 6 * [1]
+    model.fix(1, *fixed)
+
+
+    # Some reasonable properties
+    A, E, G, J, Iz, Iy = 0.18, 30e9, 12e9, 0.01, 0.005, 0.005
+
+    model.section("ElasticFrame", 1, E=E, A=A, Iz=Iz, Iy=Iy, G=G, J=J)
+    # define elements
+    try:
+        model.element(element, 2, (2, 3), section=1, transform=1)
+    except:
+        assert True
+        model.wipe()
+        return 
+    
+    raise Exception("Expected error not raised")
+
+@pytest.mark.parametrize("transform", ["Linear", "Corotational", "PDelta"])
+def test_transform_clobber(transform):
+    # Set up the model
+    model = xara.Model(ndm=3, ndf=6)
+
+    # Define geometry transforms
+    model.geomTransf("Linear", 1, (-1, 0, 0))
+    try:
+        model.geomTransf(transform, 1, ( 0, 0, 1))
+    except:
+        assert True
+        model.wipe()
+        return
+
+    assert False, "Expected error not raised"
+
+
+@pytest.mark.parametrize("ele_type", ["PrismFrame", "forceBeamColumn", "ForceFrame"])
+def test_no_section(ele_type):
+    """
+    Ensure graceful handling of invalid section tag
+    """
+    # Set up the model
+    model = xara.Model(ndm=3, ndf=6)
+
+    model.node(1, 0, 0, 0)
+    model.node(2, 0, 5, 0)
+    model.node(3, 0, 5, 5)
+
+    fixed = 6 * [1]
+    model.fix(1, *fixed)
+
+    # Define geometry transforms
+    model.geomTransf("Linear", 1, (-1, 0, 0))
+
+    # Some reasonable properties
+    A, E, G, J, Iz, Iy = 0.18, 30e9, 12e9, 0.01, 0.005, 0.005
+
+    model.section("ElasticFrame", 1, E=E, A=A, Iz=Iz, Iy=Iy, G=G, J=J)
+
+
+    try:
+        model.element(ele_type, 1, (1, 2), section=2, transform=1)
+    except:
+        assert True
+        model.wipe()
+        return
+
+    assert False, "Expected error not raised"
+
+
+
+
+def test_clobber_section():
+    """
+    Ensure graceful handling of invalid geometric transformation vectors 
+    in 3D frame elements.
+    This segfaults upstream.
+    """
+    # Set up the model
+    model = xara.Model(ndm=3, ndf=6)
+
+    # Some reasonable properties (~300x600mm RC)
+    A, E, G, J, Iz, Iy = 0.18, 30e9, 12e9, 0.01, 0.005, 0.005
+
+    model.section("ElasticFrame", 1, E=E, A=A, Iz=Iz, Iy=Iy, G=G, J=J)
+    try:
+        model.section("ElasticFrame", 1, E=E, A=A, Iz=Iz, Iy=Iy, G=G, J=J)
+    except:
+        assert True
+        model.wipe()
+        return
+
+    assert False, "Expected error not raised"
+
